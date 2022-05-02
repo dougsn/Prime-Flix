591864cf6a5167268e97b58222592825c59662e1
Douglas da Silva Nascimento <dnascimento@enavirj.com.br>
2022-05-02 16:42:27 -0300

    Fazendo as páginas e as rotas do app

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,34 @@
+import { useEffect, useState } from 'react';
 
+import api from '../../services/api'
 
 function Home() {
+
+    //https://api.themoviedb.org/3/movie/now_playing?api_key=ebda08a010cfe98458e308d56a643804&language=pt-BR
+
+    const [filmes, setFilmes] = useState([])
+
+    useEffect(()=> {
+
+        async function loadFilmes() {
+            // O parametro da api_key é required, sem eles não da para acessar os dados da API
+            const response = await api.get("movie/now_playing", {
+                params: {
+                    api_key: "ebda08a010cfe98458e308d56a643804",
+                    language: "pt-BR"
+                }
+            })
+            // O await não deixa o código seguir, até que as informações da API sejam capturadas.
+            setFilmes(response.data.results);
+        }   
+        loadFilmes();
+
+    }, [filmes])
+
     return (
+
+
+
         <div>
             <h1>Bem vindo a Home</h1>
         </div>
---
+++ src/services/api.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+
+
+const api = axios.create({
+    baseURL: "https://api.themoviedb.org/3"
+})
+
+
+export default api;
+
+
+
+
+
+
+
+
+// Base da URL: https://api.themoviedb.org/3
+
+// URL da API https://api.themoviedb.org/3/movie/now_playing?api_key=ebda08a010cfe98458e308d56a643804&language=pt-BR
+
+
